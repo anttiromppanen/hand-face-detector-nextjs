26931e19dfd284459759d0f70302d28aeaa28d2c
Antti Romppanen <romppanen.antti@gmail.com>
2024-09-18 11:21:25 +0300

    refactor drawing loop to custom hook

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,6 @@
 "use client";
 
-import {
-  CONNECTION_COLOR,
-  LEFT_SIDE_COLOR,
-  RIGHT_SIDE_COLOR,
-  TESSELATION_COLOR,
-} from "@/const/colors";
+import useHandAndFaceDetection from "@/hooks/useHandAndFaceDetection";
 import {
   initializeHandLandmarker,
   intializeFaceLandmarker,
@@ -15,16 +10,7 @@ import {
   FaceSmileIcon,
   HandRaisedIcon,
 } from "@heroicons/react/24/solid";
-import { drawConnectors, drawLandmarks } from "@mediapipe/drawing_utils";
-import { HAND_CONNECTIONS } from "@mediapipe/hands";
-import {
-  DrawingUtils,
-  FaceLandmarker,
-  FaceLandmarkerResult,
-  HandLandmarker,
-  HandLandmarkerResult,
-} from "@mediapipe/tasks-vision";
-import { useCallback, useEffect, useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import Webcam from "react-webcam";
 import styles from "./page.module.css";
 
@@ -34,14 +20,6 @@ const cameraModeSelector = {
 };
 
 export default function Home() {
-  const [showHandLines, setShowHandLines] = useState(true);
-  const [showFaceLines, setShowFaceLines] = useState(true);
-  const [handLandmarker, setHandLandmarker] = useState<HandLandmarker | null>(
-    null
-  );
-  const [faceLandmarker, setFaceLandmarker] = useState<FaceLandmarker | null>(
-    null
-  );
   const [cameraFacingMode, setCameraFacingMode] = useState<
     "user" | "environment"
   >("user");
@@ -49,124 +27,17 @@ export default function Home() {
   const canvasRef = useRef<HTMLCanvasElement>(null);
   const lastVideoTime = useRef(-1);
 
-  const handDetectionsLoop = useCallback(() => {
-    if (
-      !handLandmarker ||
-      !faceLandmarker ||
-      !cameraRef.current ||
-      !cameraRef.current.video ||
-      !canvasRef.current
-    ) {
-      return;
-    }
-    // video element & dimensions
-    const video = cameraRef.current.video;
-    const videoHeight = video.videoHeight;
-    const videoWidth = video.videoWidth;
-
-    // set canvas dimensions
-    const canvas = canvasRef.current;
-    canvas.width = videoWidth;
-    canvas.height = videoHeight;
-
-    let handDetections: HandLandmarkerResult | null = null;
-    let faceDetections: FaceLandmarkerResult | null = null;
-    const canvasCtx = canvas.getContext("2d") as CanvasRenderingContext2D;
-    const canvasWebGl = canvas.getContext("webgl") as WebGL2RenderingContext;
-
-    const drawingUtilsForFace = new DrawingUtils(canvasCtx, canvasWebGl);
-
-    if (videoHeight && videoWidth && video.readyState >= 2) {
-      const startTimeMs = performance.now();
-
-      if (lastVideoTime.current !== video.currentTime) {
-        lastVideoTime.current = video.currentTime;
-        handDetections = handLandmarker.detectForVideo(video, startTimeMs);
-        faceDetections = faceLandmarker.detectForVideo(video, startTimeMs);
-      }
-
-      // Mirror the canvas horizontally
-      canvasCtx.scale(-1, 1);
-      canvasCtx.translate(-videoWidth, 0);
-
-      // Draw hand landmarks
-      if (handDetections?.landmarks && showHandLines) {
-        const handedness = handDetections.handedness;
-
-        for (let i = 0; i < handDetections.landmarks.length; i++) {
-          const landmarks = handDetections.landmarks[i];
-
-          // selects color for hand side, might fail for more than 2 hands
-          const handColor =
-            handedness[i][0].categoryName === "Right"
-              ? RIGHT_SIDE_COLOR
-              : LEFT_SIDE_COLOR;
-
-          drawConnectors(canvasCtx, landmarks, HAND_CONNECTIONS, {
-            color: TESSELATION_COLOR,
-            lineWidth: 3,
-            visibilityMin: -1,
-          });
-          drawLandmarks(canvasCtx, landmarks, {
-            color: handColor,
-            lineWidth: 2,
-            visibilityMin: -1,
-          });
-        }
-      }
-
-      // Draw face landmarks
-      if (faceDetections?.faceLandmarks && showFaceLines) {
-        for (const landmarks of faceDetections.faceLandmarks) {
-          drawingUtilsForFace.drawConnectors(
-            landmarks,
-            FaceLandmarker.FACE_LANDMARKS_TESSELATION,
-            { color: TESSELATION_COLOR, lineWidth: 1 }
-          );
-          drawingUtilsForFace.drawConnectors(
-            landmarks,
-            FaceLandmarker.FACE_LANDMARKS_RIGHT_EYE,
-            { color: RIGHT_SIDE_COLOR }
-          );
-          drawingUtilsForFace.drawConnectors(
-            landmarks,
-            FaceLandmarker.FACE_LANDMARKS_RIGHT_EYEBROW,
-            { color: RIGHT_SIDE_COLOR }
-          );
-          drawingUtilsForFace.drawConnectors(
-            landmarks,
-            FaceLandmarker.FACE_LANDMARKS_LEFT_EYE,
-            { color: LEFT_SIDE_COLOR }
-          );
-          drawingUtilsForFace.drawConnectors(
-            landmarks,
-            FaceLandmarker.FACE_LANDMARKS_LEFT_EYEBROW,
-            { color: LEFT_SIDE_COLOR }
-          );
-          drawingUtilsForFace.drawConnectors(
-            landmarks,
-            FaceLandmarker.FACE_LANDMARKS_FACE_OVAL,
-            { color: CONNECTION_COLOR }
-          );
-          drawingUtilsForFace.drawConnectors(
-            landmarks,
-            FaceLandmarker.FACE_LANDMARKS_LIPS,
-            { color: CONNECTION_COLOR }
-          );
-          drawingUtilsForFace.drawConnectors(
-            landmarks,
-            FaceLandmarker.FACE_LANDMARKS_RIGHT_IRIS,
-            { color: RIGHT_SIDE_COLOR }
-          );
-          drawingUtilsForFace.drawConnectors(
-            landmarks,
-            FaceLandmarker.FACE_LANDMARKS_LEFT_IRIS,
-            { color: LEFT_SIDE_COLOR }
-          );
-        }
-      }
-    }
-  }, [handLandmarker, faceLandmarker, showHandLines, showFaceLines]);
+  const {
+    handAndFaceDetectionsLoop,
+    handLandmarker,
+    faceLandmarker,
+    setFaceLandmarker,
+    setHandLandmarker,
+    setShowFaceLines,
+    setShowHandLines,
+    showFaceLines,
+    showHandLines,
+  } = useHandAndFaceDetection({ cameraRef, canvasRef, lastVideoTime });
 
   useEffect(() => {
     // Initialize Hand landmarker
@@ -177,7 +48,7 @@ export default function Home() {
         console.log("Hand Landmarker initialized");
       })();
     }
-  }, [handLandmarker]);
+  }, [handLandmarker, setHandLandmarker]);
 
   useEffect(() => {
     // Initialize Face landmarker
@@ -188,15 +59,15 @@ export default function Home() {
         console.log("Face Landmarker initialized");
       })();
     }
-  }, [faceLandmarker]);
+  }, [faceLandmarker, setFaceLandmarker]);
 
   useEffect(() => {
     const interval = setInterval(() => {
-      handDetectionsLoop();
+      handAndFaceDetectionsLoop();
     }, 10);
 
     return () => clearInterval(interval);
-  }, [handDetectionsLoop]);
+  }, [handAndFaceDetectionsLoop]);
 
   return (
     <main className={styles.container}>
---
+++ src/hooks/useHandAndFaceDetection.ts
@@ -0,0 +1,180 @@
+import {
+  CONNECTION_COLOR,
+  LEFT_SIDE_COLOR,
+  RIGHT_SIDE_COLOR,
+  TESSELATION_COLOR,
+} from "@/const/colors";
+import { drawConnectors, drawLandmarks } from "@mediapipe/drawing_utils";
+import { HAND_CONNECTIONS } from "@mediapipe/hands";
+import {
+  DrawingUtils,
+  FaceLandmarker,
+  FaceLandmarkerResult,
+  HandLandmarker,
+  HandLandmarkerResult,
+} from "@mediapipe/tasks-vision";
+import { useCallback, useState } from "react";
+import Webcam from "react-webcam";
+
+interface HandAndFaceDetectionProps {
+  cameraRef: React.RefObject<Webcam>;
+  canvasRef: React.RefObject<HTMLCanvasElement>;
+  lastVideoTime: React.MutableRefObject<number>;
+}
+
+function useHandAndFaceDetection({
+  cameraRef,
+  canvasRef,
+  lastVideoTime,
+}: HandAndFaceDetectionProps) {
+  const [showHandLines, setShowHandLines] = useState(true);
+  const [showFaceLines, setShowFaceLines] = useState(true);
+
+  const [handLandmarker, setHandLandmarker] = useState<HandLandmarker | null>(
+    null
+  );
+  const [faceLandmarker, setFaceLandmarker] = useState<FaceLandmarker | null>(
+    null
+  );
+
+  const handAndFaceDetectionsLoop = useCallback(() => {
+    if (
+      !handLandmarker ||
+      !faceLandmarker ||
+      !cameraRef.current ||
+      !cameraRef.current.video ||
+      !canvasRef.current
+    ) {
+      return;
+    }
+    // video element & dimensions
+    const video = cameraRef.current.video;
+    const videoHeight = video.videoHeight;
+    const videoWidth = video.videoWidth;
+
+    // set canvas dimensions
+    const canvas = canvasRef.current;
+    canvas.width = videoWidth;
+    canvas.height = videoHeight;
+
+    let handDetections: HandLandmarkerResult | null = null;
+    let faceDetections: FaceLandmarkerResult | null = null;
+    const canvasCtx = canvas.getContext("2d") as CanvasRenderingContext2D;
+    const canvasWebGl = canvas.getContext("webgl") as WebGL2RenderingContext;
+
+    const drawingUtilsForFace = new DrawingUtils(canvasCtx, canvasWebGl);
+
+    if (videoHeight && videoWidth && video.readyState >= 2) {
+      const startTimeMs = performance.now();
+
+      if (lastVideoTime.current !== video.currentTime) {
+        lastVideoTime.current = video.currentTime;
+        handDetections = handLandmarker.detectForVideo(video, startTimeMs);
+        faceDetections = faceLandmarker.detectForVideo(video, startTimeMs);
+      }
+
+      // Mirror the canvas horizontally
+      canvasCtx.scale(-1, 1);
+      canvasCtx.translate(-videoWidth, 0);
+
+      // Draw hand landmarks
+      if (handDetections?.landmarks && showHandLines) {
+        const handedness = handDetections.handedness;
+
+        for (let i = 0; i < handDetections.landmarks.length; i++) {
+          const landmarks = handDetections.landmarks[i];
+
+          // selects color for hand side, might fail for more than 2 hands
+          const handColor =
+            handedness[i][0].categoryName === "Right"
+              ? RIGHT_SIDE_COLOR
+              : LEFT_SIDE_COLOR;
+
+          drawConnectors(canvasCtx, landmarks, HAND_CONNECTIONS, {
+            color: TESSELATION_COLOR,
+            lineWidth: 3,
+            visibilityMin: -1,
+          });
+          drawLandmarks(canvasCtx, landmarks, {
+            color: handColor,
+            lineWidth: 1,
+            visibilityMin: -1,
+          });
+        }
+      }
+
+      // Draw face landmarks
+      if (faceDetections?.faceLandmarks && showFaceLines) {
+        for (const landmarks of faceDetections.faceLandmarks) {
+          drawingUtilsForFace.drawConnectors(
+            landmarks,
+            FaceLandmarker.FACE_LANDMARKS_TESSELATION,
+            { color: TESSELATION_COLOR, lineWidth: 1 }
+          );
+          drawingUtilsForFace.drawConnectors(
+            landmarks,
+            FaceLandmarker.FACE_LANDMARKS_RIGHT_EYE,
+            { color: RIGHT_SIDE_COLOR }
+          );
+          drawingUtilsForFace.drawConnectors(
+            landmarks,
+            FaceLandmarker.FACE_LANDMARKS_RIGHT_EYEBROW,
+            { color: RIGHT_SIDE_COLOR }
+          );
+          drawingUtilsForFace.drawConnectors(
+            landmarks,
+            FaceLandmarker.FACE_LANDMARKS_LEFT_EYE,
+            { color: LEFT_SIDE_COLOR }
+          );
+          drawingUtilsForFace.drawConnectors(
+            landmarks,
+            FaceLandmarker.FACE_LANDMARKS_LEFT_EYEBROW,
+            { color: LEFT_SIDE_COLOR }
+          );
+          drawingUtilsForFace.drawConnectors(
+            landmarks,
+            FaceLandmarker.FACE_LANDMARKS_FACE_OVAL,
+            { color: CONNECTION_COLOR }
+          );
+          drawingUtilsForFace.drawConnectors(
+            landmarks,
+            FaceLandmarker.FACE_LANDMARKS_LIPS,
+            { color: CONNECTION_COLOR }
+          );
+          drawingUtilsForFace.drawConnectors(
+            landmarks,
+            FaceLandmarker.FACE_LANDMARKS_RIGHT_IRIS,
+            { color: RIGHT_SIDE_COLOR }
+          );
+          drawingUtilsForFace.drawConnectors(
+            landmarks,
+            FaceLandmarker.FACE_LANDMARKS_LEFT_IRIS,
+            { color: LEFT_SIDE_COLOR }
+          );
+        }
+      }
+    }
+  }, [
+    cameraRef,
+    canvasRef,
+    lastVideoTime,
+    showHandLines,
+    showFaceLines,
+    handLandmarker,
+    faceLandmarker,
+  ]);
+
+  return {
+    handAndFaceDetectionsLoop,
+    handLandmarker,
+    faceLandmarker,
+    setHandLandmarker,
+    setFaceLandmarker,
+    setShowHandLines,
+    setShowFaceLines,
+    showHandLines,
+    showFaceLines,
+  };
+}
+
+export default useHandAndFaceDetection;
